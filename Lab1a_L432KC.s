b64d81b1dff01f06fbee975367a763883a17ec17
umartinkus <63037721+umartinkus@users.noreply.github.com>
2024-02-09 16:43:14 -0700

    Add files via upload

--- Lab1a_L432KC.s
+++ Lab1a_L432KC.s
@@ -15,65 +15,90 @@ PUSH {lr}
 .EQU writtenDataStart, 0x20003000 //start of writing addresses
 
 //loading memory locations into registers for read/write 
-LDR R4, =dataStart 
-LDR R6, =writtenDataStart 
+LDR R4, =dataStart
+LDR R6, =writtenDataStart
 
 //Loop through program until exit code is read
 LOOP:
-CMP R4, exitCode
-BEQ EXIT
-B VALIDATE70 //if exit code was not read, check that the value is in range
+LDR R5, [R4]
+CMP R4, 0x0D
+BEQ EXIT //exit loop when exit code is read
+B VALIDATE57 //if exit code was not read, check that the value is in range
 
 // if not convert the character to the appropriate value
 //write to memory and increment mem counter
 
 //Checking upper limit of letters
-VALIDATE70:
-LDR R5, [R4]
-EOR R5, R5, #0X20 //flips the fifth bit to zero; enables checking upper and lower at same time
-CMP R5, #0x46 //compare to decimal 70
-ITE GT
-LDR R5, errorCode //replace register value with -1 if (converted to upper) it was greater than 70
-B VALIDATE65
+
+VALIDATE102:
+CMP R5, #102 //compare to decimal 102
+BGT ERROR
+B VALIDATE97
 
 //Lower limit of letters
+VALIDATE97:
+CMP R5, #0X61 //check if value is less than 65
+BLT VALIDATE70
+B TO_UPPER
+
+TO_UPPER:
+SUB R5, R5, #32
+B LETTER_TO_HEX
+
 VALIDATE65:
-CMP R5, #0X41 //check if value is less than 65
-ITEE LT
-VALIDATE57
+CMP R5, #65
+BGE LETTER_TO_HEX
+B ERROR
+
+VALIDATE70:
+CMP R5, #70
+BGT ERROR
+B VALIDATE65
+
+LETTER_TO_HEX:
 SUB R5, R5, #0X37 //subtract decimal 55 from values that represent letters
 B WRITEMEM
 
 //Upper limit of digits
 VALIDATE57:
-CMP R5, #0X39
-ITEE GT
-LDR R5, errorCode
-SUB R5, R5, #0X30 //subtract 48 from value then compare to zero
-B VALIDATE0
+CMP R5, #0X39 //check if value is less than 57
+BGT ERROR
+B VALIDATE48
 
 //Lower limit of digits
-VALIDATE0:
-CMP R5, 0X00
-IT LT
-LDR R5, errorCode
+VALIDATE48: //check that value is greater than 48
+CMP R5, #48
+BGE DIGIT_TO_HEX
+BLT ERROR
+B VALIDATE102
+
+DIGIT_TO_HEX:
+SUB R5, R5, #48
 B WRITEMEM
 
 WRITEMEM:
-STR R5, R6 
-ADD R6, R6, #0X04
+STR R5, [R6] //store R5 in memory and shift index by 4
+ADD R6, #4
 ADD R4, R4, #0X04
 B LOOP
 
+ERROR:
+MOV R5, #-1 //replace register value with -1 if (converted to upper) it was greater than 70
+STR R5, [R6], +0x04
+ADD R4, R4, #0X04
+B LOOP
 
 //Exit sequence
 EXIT:
 POP {PC}
 
+/*
 .data
 exitCode:
 .LONG 0x0D
 errorCode:
 .LONG 0xFFFFFFFF
+*/
+
 /*--------------------------------------*/
 
