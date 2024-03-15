0084c66def6ecef8df5170894e720e08f3ee6f50
Drake Boulianne <dboul63@gmail.com>
2024-03-15 16:40:27 -0600

    updated part b solution

--- L3/Lab3b.s
+++ L3/Lab3b.s
@@ -23,15 +23,14 @@ Sort:
 /*-----------------Students write their subroutine here--------------------*/
 
 INIT:
-PUSH{LR} // Must pop LR to PC at end of subroutine to return POP{PC}
-PUSH{R0} // For size
-PUSH{R1} // For curr pointer
-PUSH{R3} // For Truth var
-PUSH{R4} //For Counter
-PUSH{R5} // to store curr temp data
-PUSH{R6} // To store next temp data
-
-LDR R0, [SP, #-32]
+PUSH {LR} // Must pop LR to PC at end of subroutine to return POP{PC}
+PUSH {R0} // For size
+PUSH {R1} // For curr pointer
+PUSH {R3} // For Truth var
+PUSH {R4} // For Counter
+PUSH {R5} // to store curr temp data
+PUSH {R6} // To store next temp data
+LDR R0, [SP, #28]
 LDR R1, =0x20001000 // Init pointer
 MOV R3, #0 // register for the sorted flag
 MOV R4, R0 // Init counter to size.
@@ -39,9 +38,10 @@ MOV R4, R0 // Init counter to size.
 loopRST:
 CMP R3, #1
 BEQ END
+
 // Reset pointers to the start of the data
 LDR R1, =0x20001000 // Init Curr pointer
-MOV R4, R0 // reset counter to zero
+SUB R4, R0, #1 // reset counter to size -1
 MOV R3, #1 // Start assuming sorted
 
 MAINLOOP:
@@ -56,31 +56,29 @@ B LOW
 LOW:
 // Executes if lower than or equal
 // No swapping occurs, Increments pointer, Decrement counter
-SUB R4, #-1
+SUB R4, #1
 ADD R1, #4
 B MAINLOOP
+
 HIGH:
 // Executes if greater than condition is met
 // Swapping occurs, Increments pointer, Decrements counter
 MOV R3, #0 // Set Sorted flag to false
 STR R5, [R1, #4] //Swap Two values in memory
 STR R6, [R1]
-SUB R4, #-1 // Decrement counter
+SUB R4, #1 // Decrement counter
 ADD R1, #4 // Increment data pointer
 B MAINLOOP
 
-
-
-
-
 END:
-POP{R6}
-POP{R5}
-POP{R4}
-POP{R3}
-POP{R1}
-POP{R0}
-POP{PC}
+POP {R6}
+POP {R5}
+POP {R4}
+POP {R3}
+POP {R1}
+POP {R0}
+POP {PC}
+
 
 
 /*-------Code ends here ---------------------*/
