3bf863915c4a9f816b017155a9b99def9e7d46ab
Drake Boulianne <dboul63@gmail.com>
2024-03-15 17:29:18 -0600

    updated lab3c

--- L3/Lab3c_L432KC.s
+++ L3/Lab3c_L432KC.s
@@ -1,52 +1,64 @@
 /*Author - Wing Hoy. Last edited on Jan 17, 2022 */
+
 /*-----------------DO NOT MODIFY--------*/
+
 .global Display
+
 .global printf
+
 .global cr
+
 .extern value
+
 .extern getstring
+
 .syntax unified
 
+
+
 .text
+
 Display:
 /*-----------------Students write their subroutine here--------------------*/
 
 PUSH {LR}
-PUSH {R0} //register used to print values
-PUSH {R1} //used as a counter
-PUSH {R2} //used to hold memory address of stored values
-PUSH {R3} //Number of values
+PUSH {R0}
+PUSH {R4} //register used to print values
+PUSH {R7} //used as a counter
+PUSH {R5} //used to hold memory address of stored values
+PUSH {R6} //Number of values
 
-LDR R0, =Hello //load r0 with first string
-bl printf
-bl cr //print hello string and start a new line
-MOV R1, #0 //set counter to zero
-LDR R2, [SP, #-24] //load R2 with address of array
-LDR R3, [SP, #-20] //load R3 with number of vals
+LDR R0, =Hello //load R4 with first string
+bl printfbl cr //print hello string and start a new line
+MOV R7, #0 //set counter to zero
+LDR R5, [SP, #24] //load R5 with address of array
+LDR R6, [SP, #20] //load R6 with number of vals
 
 LDR R0, =Entries
 bl printf
-MOV R0, R3 //load r0 with number of values
+MOV R0, R6 //load R4 with number of values
 bl value // print to terminal
 bl cr
 LDR R0, =Array
 bl printf
 bl cr
 
+
 /*The following loop goes through array values and prints them until all values have been printed*/
 LOOP:
-CMP R1, R3 //check if we've read all values
+CMP R7, R6 //check if we've read all values
 BGE EXIT //go to exit routine if we've read all values
-LDR R0, [R2], #4 //move first array value into R0 and increment by 4
-bl printf
+LDR R0, [R5], #4 //move first array value into R4 and increment by 4
+bl value
 bl cr
-ADD R1, #1 //incremend counter by 1
+ADD R7, #1 //incremend counter by 1
 b LOOP //go back to the start of the loop
 
 EXIT:
-POP {R3}
-POP {R2}
-POP {R1}
+POP {R6}
+POP {R5}
+POP {R7}
+POP {R4}
 POP {R0}
 POP {PC}
 
