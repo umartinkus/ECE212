6cfe0a49b165db0d48877f8e39e881334b07a5ae
DrakBoul <91000709+DrakBoul@users.noreply.github.com>
2024-03-27 10:24:16 -0600

    Updated Lab3b.s

--- L3/Lab3b.s
+++ L3/Lab3b.s
@@ -24,59 +24,59 @@ Sort:
 
 INIT:
 PUSH {LR} // Must pop LR to PC at end of subroutine to return POP{PC}
-PUSH {R0} // For size
 PUSH {R1} // For curr pointer
 PUSH {R3} // For Truth var
 PUSH {R4} // For Counter
 PUSH {R5} // to store curr temp data
 PUSH {R6} // To store next temp data
-LDR R0, [SP, #28]
-LDR R1, =0x20001000 // Init pointer
-MOV R3, #0 // register for the sorted flag
-MOV R4, R0 // Init counter to size.
+PUSH {R7} // to store size of the data
+LDR R7, [SP, #28] // holding the size o the data
 
-loopRST:
-CMP R3, #1
-BEQ END
+MOV R3, #0 // register for the sorted flag start assuming unsorted
+
+loopRST: // outer loop
+CMP R3, #1 // check to see if the array is sorted
+BEQ END // if sorted branch to end of the program
 
 // Reset pointers to the start of the data
-LDR R1, =0x20001000 // Init Curr pointer
-SUB R4, R0, #1 // reset counter to size -1
+MOV R1, R0 // Init Curr pointer to first element in array
+SUB R4, R7, #1 // reset counter to size of array less one
 MOV R3, #1 // Start assuming sorted
 
 MAINLOOP:
 CMP R4, #0 // end of array condition
-BEQ loopRST
-LDR R5, [R1] // Curr
-LDR R6, [R1, #4] // Next
-CMP R5, R6
-BGT HIGH //
-B LOW
+BEQ loopRST // branch to outer loop to reset loop condition if end of array
+LDR R5, [R1] // Curr element in array
+LDR R6, [R1, #4] // Next element in array
+CMP R5, R6 // compare curr and next elements 
+BGT HIGH // swap elements if Curr > Next
+B LOW // Otherwise dont swap
 
 LOW:
 // Executes if lower than or equal
 // No swapping occurs, Increments pointer, Decrement counter
-SUB R4, #1
-ADD R1, #4
-B MAINLOOP
+SUB R4, #1 // decrement counter 
+ADD R1, #4 // increment pointer
+B MAINLOOP // branch back to main loop
 
 HIGH:
 // Executes if greater than condition is met
 // Swapping occurs, Increments pointer, Decrements counter
 MOV R3, #0 // Set Sorted flag to false
-STR R5, [R1, #4] //Swap Two values in memory
-STR R6, [R1]
+STR R5, [R1, #4] // change curr to next's memory location
+STR R6, [R1]  // change next to currs memory location
 SUB R4, #1 // Decrement counter
 ADD R1, #4 // Increment data pointer
-B MAINLOOP
+B MAINLOOP // branch back to main loop
 
 END:
+// pop registers off the stack
+POP {R7}
 POP {R6}
 POP {R5}
 POP {R4}
 POP {R3}
 POP {R1}
-POP {R0}
 POP {PC}
 
 
