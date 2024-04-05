31010d00a6dcce92081c3ddea714aa58c1332542
DrakBoul <91000709+DrakBoul@users.noreply.github.com>
2024-04-05 15:22:16 -0600

    Update Lab4a.s

--- L4/Lab4a.s
+++ L4/Lab4a.s
@@ -14,11 +14,13 @@ Welcomeprompt:
 /*--------------------------------------*/
 PUSH {LR}
 PUSH {R0}
+PUSH {R7}
 
 MAIN:
 LDR R0, =prompt //load the prompt for user to enter a value
 bl printf
 bl getchar //entered character is now in R0
+MOV R7, R0 // Load character into R7
 CMP R0, #90 //check if char is greater than ASCII Z
 BGT ERROR
 CMP R0, #65 //check if char greater than ASCII A
@@ -26,20 +28,24 @@ BGE EXIT //character entered was an uppercase letter
 CMP R0, #57 //check if greater than 57 (9) or less than 65 (A)
 BGT ERROR
 CMP R0, #48 //check if 48 <= char <= 57
-BGT EXIT //go to exit sequence if in valid range
+BGE EXIT //go to exit sequence if in valid range
 B ERROR //throw error if not in valid range
 
 ERROR:
+BL printf
 LDR R0, =error
 bl printf
 bl cr
 B MAIN
 
 EXIT:
-bl printf
-STR R0, [SP, #8]
+BL cr
+MOV R0, R7
+BL putchar
+STR R7, [SP, #12]
+POP {R7}
 POP {R0}
-POP {LR}
+POP {PC}
 /*-------Students write their code here ------------*/
 
 /*-----------------DO NOT MODIFY--------*/
@@ -49,4 +55,3 @@ prompt:
 error:
 .string "Invalid entry. Try again."
 /*--------------------------------------*/
-
